1b9125fef04e5ed74ecccda1f30766da4d5e7c07
JamieJY <98108262+JamieJY@users.noreply.github.com>
2023-10-14 22:54:39 -0400

    Time: 4 ms (74.5%), Space: 41.6 MB (96.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,42 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
-        //return false if the length of two string are different
+       
         if(s.length() != t.length()){
             return false;
         }
-      
-       // use map to store char and its count
-        Map<Character,Integer> charAndNum = new HashMap<>();
-       
-        //loop first string s and add to map
-        for(int i = 0; i < s.length(); i++){
-            if(charAndNum.containsKey(s.charAt(i))){
-                charAndNum.put(s.charAt(i),charAndNum.get(s.charAt(i))+1);
-            }
-            else{
-                charAndNum.put(s.charAt(i),1);
-                }
-            }
-        
-        //loop second string t and subtract char count from map
-         for(int i = 0; i < s.length(); i++){
-            if(charAndNum.containsKey(t.charAt(i))){
-                charAndNum.put(t.charAt(i),charAndNum.get(t.charAt(i))-1);
-            }
-            else{
-                return false;
-               }
-            }
-        
-        // check if all the count is zero or not
-        for(Character alphabet : charAndNum.keySet()){
-            if(charAndNum.get(alphabet) != 0){
-                return false;
-            }
+       //create int array with 26 length to record the count of alphabet a-z
+       // index 0->a,index 1->b, 2->c, ....,25->z
+
+         int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
 
-        return true;
-        
-    }
+        //check if each count is zero or not 
+        for(int num :count){
+            if(num != 0){
+                return false;
+            }          
+        } 
+         return true;   
+    }     
 }
